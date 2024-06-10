f6f6a3ef91c24e544380d3ab37868c1a97c3bcca
MatyasLukacs <matyas.lukacs@adweko.com>
2024-06-10 19:00:17 +0200

    simple routing

--- src/main.js
+++ src/main.js
@@ -4,3 +4,14 @@ import { SERVER, PORT } from "./server";
 SERVER.listen(PORT, () => {
     console.log("Server has started")
 });
+
+SERVER.use(
+    (req, res, next) => {
+    res.status(404).json({
+        status: "Page not found"
+    })
+})
+
+SERVER.get("/", (req, res) => {
+    res.send("Hello World");
+});
