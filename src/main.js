2b8e74fad8bd74411591f9e7224bb5c9ce3e600c
MatyasLukacs <matyas.lukacs@adweko.com>
2024-07-03 10:51:37 +0200

    adding index+webshop views

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 "use strict"
 import { SERVER, PORT } from "./server";
+import MainLayouts from "express-ejs-layouts";
+import express from "express";
 
+//template engine
+SERVER.use(MainLayouts);
+SERVER.set("layout", "layouts/html");
+SERVER.set("view engine", "ejs");
+
+//server connection
 SERVER.listen(PORT, () => {
     console.log("Server has started")
+});
+
+//static files
+SERVER.use(express.static("views/static"));
+
+SERVER.get("/", (req, res) => {
+    res.render("pages/index", {
+        title: "NeoLife Shop - Gáspár Zsuzsanna"
+    });
+});
+
+SERVER.get("/webshop", (req, res) => {
+    res.render("pages/webshop", {
+        title: "Webshop"
+    })
 });
 
 SERVER.use(
@@ -10,8 +33,4 @@ SERVER.use(
     res.status(404).json({
         status: "Page not found"
     })
-})
-
-SERVER.get("/", (req, res) => {
-    res.send("Hello World");
 });
